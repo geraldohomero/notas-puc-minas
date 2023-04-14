219172cb2fcbb46f5bfa8806799b11afe5f9f994
Geraldo Homero <geraldohomero@protonmail.com>
2023-04-14 13:41:09 -0300

    update

---
+++ Eixo-2/Desenvolvimento Back-end/ORM-Exemplo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ORM_Exemplo
+{
+
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public ICollection<Filme> Filme { get; set; }
+    } 
+
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int GeneroId { get; set; }
+        [ForeignKeyAttribute("GeneroId")]
+        public Genero Genero { get; set; }
+    }
+
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Filme> Filme { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=./SQLEXPRESS;Database=orm;Trusted_Connection=True;");
+        }
+
+    
+    }
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
